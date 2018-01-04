3c1d2586fc9b20a38d4c0f4d4a669638c4467719
hpq <hepeiqin@casachina.com.cn>
2018-01-04 15:40:28 +0800

    创建 /syncthing 前检查是否存在

--- docker/projects/file-manager/syncthing/ctrl/create_container.sh
+++ docker/projects/file-manager/syncthing/ctrl/create_container.sh
@@ -2,7 +2,7 @@
 
 RUN_USER=vcube
 SHARE_PATH="/syncthing"
-mkdir $SHARE_PATH
+[ -d $SHARE_PATH ] || mkdir $SHARE_PATH
 chmod 777 $SHARE_PATH
 
 docker run --name=syncthing --restart=always -d -v $SHARE_PATH:/home/$RUN_USER -p 8384:8384 -p 22000:22000 -it syncthing
