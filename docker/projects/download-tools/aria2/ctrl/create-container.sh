66aa2cc72375c4a96e02bbeb294f9e8f47e11094
lightime <lightimehpq@gmail.com>
2018-01-07 16:33:26 +0000

    set aria2 to be restarted when reboot

--- docker/projects/download-tools/aria2/ctrl/create-container.sh
+++ docker/projects/download-tools/aria2/ctrl/create-container.sh
@@ -7,4 +7,4 @@ echo IMAGE: $IMAGE
 echo CONTAINER: $CONTAINER
 echo INER_DOWNLOAD_PATH: $INER_DOWNLOAD_PATH
 
-docker run --name=$CONTAINER -d -v /media/nas-2/download/aria2:$INER_DOWNLOAD_PATH -p 6800:6800 -p 6789:80 -it $IMAGE
+docker run --name=$CONTAINER -d --restart=always -v /media/nas-2/download/aria2:$INER_DOWNLOAD_PATH -p 6800:6800 -p 6789:80 -it $IMAGE
