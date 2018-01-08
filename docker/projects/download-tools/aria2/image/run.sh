002e0f1e6d494e1eb41a14857c978dd3cb107ac0
lightime <lightimehpq@gmail.com>
2018-01-08 16:47:08 +0000

    save configuration of aria2 when it exit and recover when it restart

--- docker/projects/download-tools/aria2/image/run.sh
+++ docker/projects/download-tools/aria2/image/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-aria2c --conf-path=/etc/aria2c.conf
+aria2c --conf-path=/download/.aria2/aria2c.conf --input-file=/download/.aria2/aria2.session
 darkhttpd /var/www/html --log /var/log/darkhttpd/darkhttpd.log --daemon
 
 /bin/sh
