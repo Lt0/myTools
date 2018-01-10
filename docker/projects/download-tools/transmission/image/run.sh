e57dce85934da902a0c55c42b96292a6ec4f7c9e
hpq <hepeiqin@casachina.com.cn>
2018-01-10 17:36:25 +0800

    transmission: enable auto_add feature

--- docker/projects/download-tools/transmission/image/run.sh
+++ docker/projects/download-tools/transmission/image/run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-transmission-daemon --config-dir /download/.transmission/ --logfile /var/log/transmission/transmission.log
+[ -d "/download/.transmission/" ] || mkdir -p /download/.transmission/
+[ -d "/var/log/transmission" ] || mkdir -p /var/log/transmission
+[ -d "/download/auto_add" ] || mkdir -p /download/auto_add
+
+transmission-daemon --config-dir /download/.transmission/ --logfile /var/log/transmission/transmission.log --watch-dir /download/auto_add
 /bin/sh
