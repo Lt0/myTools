38f962b09b94750d9c19294a93b84b2423a7c3b3
hpq <hepeiqin@casachina.com.cn>
2018-01-10 23:18:40 +0800

    transmission: save downloaded task to complete

--- docker/projects/download-tools/transmission/image/run.sh
+++ docker/projects/download-tools/transmission/image/run.sh
@@ -3,6 +3,8 @@
 [ -d "/download/.transmission/" ] || mkdir -p /download/.transmission/
 [ -d "/var/log/transmission" ] || mkdir -p /var/log/transmission
 [ -d "/download/auto_add" ] || mkdir -p /download/auto_add
+[ -d "/download/complete" ] || mkdir -p /download/complete
+[ -d "/download/incomplete" ] || mkdir -p /download/incomplete
 
 transmission-daemon --config-dir /download/.transmission/ --logfile /var/log/transmission/transmission.log --watch-dir /download/auto_add
 /bin/sh
