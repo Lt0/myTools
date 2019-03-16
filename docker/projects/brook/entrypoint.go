39e07d8e2bfe73694aa9317c38c8d317754c3352
Lt0 <lightimehpq@gmail.com>
2019-03-16 12:31:38 +0000

    add ssclient details for docker project brook

--- docker/projects/brook/entrypoint.go
+++ docker/projects/brook/entrypoint.go
@@ -45,5 +45,10 @@ Usage:
 
   Start a ssserver:
     docker run --name="ssserver" -d --restart=always --net=host brook ssserver -l :your_ssserver_port -p your_ssserver_password
+
+  Start a ssclient:
+    docker run --name="ssclient" -d --restart=always --net=host brook ssclient -l 127.0.0.1:8080 -i 127.0.0.1 -s ssserver_ip:ssserver_port -p ssserver_password --http
+    export http_proxy=127.0.0.1:8080
+    export https_proxy=127.0.0.1:8080
 	`)
 }
