591b39aae4cb95f6c688c776faa9bf8079a00e60
git config --global user.name <xiehaohong@casachina.com.cn>
2019-08-09 08:55:19 +0000

    add docker project bionix-lxde

---
+++ docker/projects/desktop/bionic-lxde/entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+IMG=bionic-lxde
+CTN=bionic-lxde
+
+show_help() {
+echo "
+Start desktop:
+  docker run -d --name $CTN -p <vnc-port>:5901 -p <web-vnc-port>:6901 -it $IMG start
+
+  e.g:
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -it $IMG start
+
+
+
+Start desktop and set geometry:
+  docker run -d --name $CTN -p <vnc-port>:5901 -p <web-vnc-port>:6901 -e geometry=<WIDTH>x<HEIGHT> -it $IMG start
+
+  e.g:
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=1920x1080 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=1600x900 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=1366x768 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=1280x1024 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=1152x864 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=1024x768 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=800x600 -it $IMG start
+
+
+Change vncserver password:
+  docker exec -it $CTN tightvncpasswd
+
+
+Note:
+  1. Default geometry is 1920x1080
+  2. Default passwd is lightime
+"
+}
+
+start_desktop() {
+	echo "$(date) - Start desktop with $geometry"
+	vncserver -geometry $geometry
+	websockify -D --web=/usr/share/novnc/ 6901 localhost:5901
+	bash
+}
+
+case $1 in
+	start )
+		start_desktop
+	;;
+	* )
+		show_help
+	;;
+esac
