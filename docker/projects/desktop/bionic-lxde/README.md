d09989b163c6ec01c5fa7e12d7c80b5d958e6fe4
lightime <lightimehpq@gmail.com>
2019-08-09 19:08:50 +0000

    support RPD and fix some bug

---
+++ docker/projects/desktop/bionic-lxde/README.md
@@ -0,0 +1,2 @@
+# Description
+LXDE descktop based on ubuntu 18.04 and running in container. You can connect this desktop by VNC/RPD client or web
--- docker/projects/desktop/bionic-lxde/entrypoint.sh
+++ docker/projects/desktop/bionic-lxde/entrypoint.sh
@@ -6,28 +6,32 @@ CTN=bionic-lxde
 show_help() {
 echo "
 Start desktop:
-  docker run -d --name $CTN -p <vnc-port>:5901 -p <web-vnc-port>:6901 -it $IMG start
+  docker run -d --name $CTN -p <vnc-port>:5901 -p <web-vnc-port>:6901 -p <xrpd-port>:3389 -it $IMG start
 
   e.g:
-    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -p 3389:3389 -it $IMG start
 
 
 
 Start desktop and set geometry:
-  docker run -d --name $CTN -p <vnc-port>:5901 -p <web-vnc-port>:6901 -e geometry=<WIDTH>x<HEIGHT> -it $IMG start
+  docker run -d --name $CTN -p <vnc-port>:5901 -p <web-vnc-port>:6901 -p <xrpd-port>:3389 -e geometry=<WIDTH>x<HEIGHT> -it $IMG start
 
   e.g:
-    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=1920x1080 -it $IMG start
-    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=1600x900 -it $IMG start
-    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=1366x768 -it $IMG start
-    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=1280x1024 -it $IMG start
-    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=1152x864 -it $IMG start
-    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=1024x768 -it $IMG start
-    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -e geometry=800x600 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -p 3389:3389 -e geometry=1920x1080 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -p 3389:3389 -e geometry=1600x900 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -p 3389:3389 -e geometry=1366x768 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -p 3389:3389 -e geometry=1280x1024 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -p 3389:3389 -e geometry=1152x864 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -p 3389:3389 -e geometry=1024x768 -it $IMG start
+    docker run -d --name $CTN -p 5901:5901 -p 6901:6901 -p 3389:3389 -e geometry=800x600 -it $IMG start
+
+
+Start desktop with customized vnc password:
+  docker run -d --name $CTN -p <vnc-port>:5901 -p <web-vnc-port>:6901 -p <xrpd-port>:3389 -e VNC_PASSWD=<you_password> -it $IMG start
 
 
 Change vncserver password:
-  docker exec -it $CTN tightvncpasswd
+  docker exec -it $CTN vncpasswd
 
 
 Note:
@@ -37,9 +41,18 @@ Note:
 }
 
 start_desktop() {
-	echo "$(date) - Start desktop with $geometry"
-	vncserver -geometry $geometry
+	echo "$(date) - Start VNC desktop with $geometry"
+	vncserver -geometry $geometry << !PWD!
+$VNC_PASSWD
+$VNC_PASSWD
+!PWD!
+
+	echo "$(date) - Start VNC Web Server"
 	websockify -D --web=/usr/share/novnc/ 6901 localhost:5901
+
+	echo "$(date) - Start XRDP"
+	xrdp
+
 	bash
 }
 
