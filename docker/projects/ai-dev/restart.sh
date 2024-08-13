7dcf89fe414d8cde94744089f49d8dadb00ab48a
hepq <hepq@xiaopeng.com>
2024-08-12 20:22:43 -0700

    ai-dev: support sshd

--- docker/projects/ai-dev/restart.sh
+++ docker/projects/ai-dev/restart.sh
@@ -2,4 +2,5 @@
 
 docker rm -f ai-dev
 
-docker run --name ai-dev --restart always --net host -d -v $HOME/projects/ai-dev:/root/ai-dev -w /root/ai-dev -it lightimehpq/ai-dev
+#docker run --name ai-dev --restart always --net host -d -v $HOME/projects/ai-dev:/root/ai-dev -w /root/ai-dev -it lightimehpq/ai-dev
+docker run --name ai-dev --restart always -p 2222:2222 -d -v $HOME/projects/ai-dev:/root/ai-dev -w /root/ai-dev -it lightimehpq/ai-dev
